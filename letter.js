df4e7a404cc06e025623bfc7c0b79fdbe9500fd0
leonardbbacon-coder <leonardbbacon@gmail.com>
2026-02-07 21:17:10 +0800

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,30 @@
+const letter = `I just want you to know something…
+
+Thank you for being you.
+You make my days lighter, and my heart calmer.
+I’m really happy I get to talk to you.
+I really hope that someday we'll be together forever.
+I'm really glad that you have become a person in my life.
+I hope that no matter what happens we'll be able to get through
+it all I love you so much. Thank you for everything
+
+Happy Valentine’s 💗
+— Leo`;
+
+const letterText = document.getElementById("letterText");
+const openFlowers = document.getElementById("openFlowers");
+
+let i = 0;
+
+function typeLetter(){
+  if(i < letter.length){
+    letterText.textContent += letter[i];
+    i++;
+    setTimeout(typeLetter, 20);
+  }
+}
+typeLetter();
+
+openFlowers.addEventListener("click", () => {
+  window.location.href = "flower.html";
+});
